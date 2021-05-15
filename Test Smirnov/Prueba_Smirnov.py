6d24cd9cebe1b63ababd6ca8ecd03b76f36cda99
Jordi <jordi8101@gmail.com>
2021-05-15 13:43:16 -0700

    Prueba smirnov casi terminada

--- Test Smirnov/Prueba_Smirnov.py
+++ Test Smirnov/Prueba_Smirnov.py
@@ -22,24 +22,27 @@ def clickSolve():
     Label3.configure(text=f"El valor Di de tablas es: {str(result['aprox'])}")
 
 
-Label(main, text="Prueba de kolmogorov-smirnov", font=("Arial", 14)).pack()
-Label(main, text="Inserte el numero de alpha: ").pack()
+Label(main, text="Prueba de kolmogorov-smirnov",
+      font=("Arial", 14)).grid(row=0, column=0, sticky='LEFT')
+Label(main, text="Inserte el numero de alpha: ").grid(row=1, column=0)
 
 TxtAlpha = tk.Entry(main)
-TxtAlpha.pack()
+TxtAlpha.grid(row=2, column=0)
 
-Label(main, text="Inserte el numero de numeros que desea utilizar: ").pack()
+Label(main, text="Inserte el numero de numeros que desea utilizar: ").grid(
+    row=3, column=0)
 
 txtN = tk.Entry(main)
-txtN.pack()
+txtN.grid(row=4, column=0)
 
-btnSolve = tk.Button(main, text="Calcular", command=clickSolve).pack()
+btnSolve = tk.Button(main, text="Calcular",
+                     command=clickSolve).grid(row=5, column=0)
 # ///*btnSolve.pack()*/
 
-Label1.pack()
+# Label1.place(x=10, y=100)
 
-Label2.pack()
+# Label2.place(x=10, y=110)
 
-Label3.pack()
+# Label3.place(x=10, y=120)
 
 main.mainloop()
