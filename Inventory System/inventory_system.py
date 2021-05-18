2a212ceaad6c0bb050cf640f68f6e6112baa26e4
jordi <jordi8101@gmail.com>
2021-05-18 00:24:50 -0700

    Inventory system script finished

--- Inventory System/inventory_system.py
+++ Inventory System/inventory_system.py
@@ -1,77 +1,351 @@
 import pandas as pd
+from scripts.generator import *
+import random
 
 
 class inventory_system:
+    # Data for initial values
     month_demand = {
-        "qty": [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
-        "prob": [0.010, 0.015, 0.020, 0.020, 0.022, 0.023, 0.025, 0.027, 0.028, 0.029, 0.035,  0.045, 0.060, 0.065, 0.070, 0.080, 0.075, 0.070, 0.065, 0.060, 0.050, 0.040, 0.030, 0.016, 0.015, 0.005],
+        "qty": [
+            35,
+            36,
+            37,
+            38,
+            39,
+            40,
+            41,
+            42,
+            43,
+            44,
+            45,
+            46,
+            47,
+            48,
+            49,
+            50,
+            51,
+            52,
+            53,
+            54,
+            55,
+            56,
+            57,
+            58,
+            59,
+            60,
+        ],
+        "prob": [
+            0.010,
+            0.015,
+            0.020,
+            0.020,
+            0.022,
+            0.023,
+            0.025,
+            0.027,
+            0.028,
+            0.029,
+            0.035,
+            0.045,
+            0.060,
+            0.065,
+            0.070,
+            0.080,
+            0.075,
+            0.070,
+            0.065,
+            0.060,
+            0.050,
+            0.040,
+            0.030,
+            0.016,
+            0.015,
+            0.005,
+        ],
     }
-    prob_delivery_months = {
-        "months": [1, 2, 3],
-        "prob": [0.30, 0.40, 0.30]
-    }
+    prob_delivery_months = {"months": [1, 2, 3], "prob": [0.30, 0.40, 0.30]}
     seasonal_factors = {
         "month": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-        "factor": [1.90, 1.00, 0.90, 0.80, 0.80, 0.70, 0.80, 0.90, 1.00, 1.20, 1.30, 1.40]
+        "factor": [
+            1.20,
+            1.00,
+            0.90,
+            0.80,
+            0.80,
+            0.70,
+            0.80,
+            0.90,
+            1.00,
+            1.20,
+            1.30,
+            1.40,
+        ],
     }
     initial_values = {
-        "inventory_initial": 150,
+        "inv_initial": 150,
         "order_cost": 50,
         "inventory_cost": 26,
         "cost_absences": 25,
         "days_year": 260,
         "month_demand": month_demand,
         "prob_delivery_months": prob_delivery_months,
-        "seasonal_factors": seasonal_factors
+        "seasonal_factors": seasonal_factors,
     }
+    # Data for simulation
+    cumulative_probability_delivery = {"months": [], "L_inf": [], "L_sup": []}
+    cumulative_probability = {"qty": [], "L_inf": [], "L_sup": []}
+    Q = 200
+    R = 100
+    # Data for generator
+    data_generator = {"n": [], "Xn": [], "Xn+1": [], "Rn": []}
+    arraySeed = []
+    counter = 0
+    X0 = 17.00
+    a = 101.00
+    c = 221.00
+    m = 17001.00
+    stop = False
+    # End data for generator
 
-    def calc_probability_delivery(self):
-        cumulative_probability_delivery = {
-            "months": [],
-            "L_inf": [],
-            "L_sup": []
-        }
+    # Data for simulation awaiting
+    data_simulation = {
+        "month": [],
+        "reorder": [],
+        "inv_initial": [],
+        "Ri": [],
+        "dem_sim": [],
+        "seasonal_factors": [],
+        "dem_adjust": [],
+        "inv_final": [],
+        "missing": [],
+        "order": [],
+        "inv_monthly": [],
+        "Ri2": [],
+        "month_delivered": [],
+    }
+    # Constructor
+    def __init__(self):
+        Recursivo(
+            self.X0,
+            self.a,
+            self.c,
+            self.m,
+            self.counter,
+            self.stop,
+            self.arraySeed,
+            self.data_generator,
+        )
+
+    # Methods for limits
+    def Calc_probability_delivery(self):
+        self.cumulative_probability_delivery = {"months": [], "L_inf": [], "L_sup": []}
+
         for i in range(0, len(self.initial_values["prob_delivery_months"]["months"])):
-            cumulative_probability_delivery["months"].append(
-                self.initial_values["prob_delivery_months"]["months"][i])
+            self.cumulative_probability_delivery["months"].append(
+                self.initial_values["prob_delivery_months"]["months"][i]
+            )
             if i == 0:
-                cumulative_probability_delivery["L_sup"].append(
+                self.cumulative_probability_delivery["L_sup"].append(
                     self.initial_values["prob_delivery_months"]["prob"][i]
                 )
-                cumulative_probability_delivery["L_inf"].append(0)
+                self.cumulative_probability_delivery["L_inf"].append(0)
             else:
-                cumulative_probability_delivery["L_sup"].append(
-                    cumulative_probability_delivery["L_sup"][i-1] +
-                    self.initial_values["prob_delivery_months"]["prob"][i]
+                self.cumulative_probability_delivery["L_sup"].append(
+                    self.cumulative_probability_delivery["L_sup"][i - 1]
+                    + self.initial_values["prob_delivery_months"]["prob"][i]
                 )
-                cumulative_probability_delivery["L_inf"].append(
-                    cumulative_probability_delivery["L_sup"][i-1]
+                self.cumulative_probability_delivery["L_inf"].append(
+                    self.cumulative_probability_delivery["L_sup"][i - 1]
                 )
-        df = pd.DataFrame(cumulative_probability_delivery)
-        print(df)
 
-    def calc_probability_month(self):
-        cumulative_probability = {
-            "qty": [],
-            "L_inf": [],
-            "L_sup": []
-        }
+    def Calc_probability_month(self):
+        self.cumulative_probability = {"qty": [], "L_inf": [], "L_sup": []}
         for i in range(0, len(self.initial_values["month_demand"]["qty"])):
-            cumulative_probability["qty"].append(
-                self.initial_values["month_demand"]["qty"][i])
+            self.cumulative_probability["qty"].append(
+                self.initial_values["month_demand"]["qty"][i]
+            )
             if i == 0:
-                cumulative_probability["L_sup"].append(
-                    self.initial_values["month_demand"]["prob"][i])
-                cumulative_probability["L_inf"].append(0)
+                self.cumulative_probability["L_sup"].append(
+                    self.initial_values["month_demand"]["prob"][i]
+                )
+                self.cumulative_probability["L_inf"].append(0)
             else:
-                cumulative_probability["L_sup"].append(
-                    cumulative_probability["L_sup"][i-1] +
-                    self.initial_values["month_demand"]["prob"][i])
-                cumulative_probability["L_inf"].append(
-                    cumulative_probability["L_sup"][i-1])
-        # df = pd.DataFrame(cumulative_probability)
+                self.cumulative_probability["L_sup"].append(
+                    self.cumulative_probability["L_sup"][i - 1]
+                    + self.initial_values["month_demand"]["prob"][i]
+                )
+                self.cumulative_probability["L_inf"].append(
+                    self.cumulative_probability["L_sup"][i - 1]
+                )
+        # df = pd.DataFrame(self.cumulative_probability)
         # print(df)
+
+    orderActive = False
+    limitMonth = None
+    counterMonth = None
+    missingQty = 0
+    counterRn = 0
+    # Main simulation
+    def Simulation(self):
+        try:
+            # We simulate 12 months (1 year)
+            for y in range(0, 12):
+                # We take 1 num pseudo from the generator
+                num_pseudo = self.data_generator["Rn"][self.counterRn]
+                self.counterRn += 1
+                if y == 0:
+                    self.data_simulation["inv_initial"].append(
+                        self.initial_values["inv_initial"]
+                    )
+                else:
+                    self.data_simulation["inv_initial"].append(new_inv)
+
+                self.data_simulation["month"].append(y + 1)
+                self.data_simulation["seasonal_factors"].append(
+                    self.initial_values["seasonal_factors"]["factor"][y]
+                )
+                self.data_simulation["reorder"].append(self.R)
+                self.data_simulation["Ri"].append(num_pseudo)
+
+                for i in range(0, len(self.cumulative_probability["qty"])):
+                    l_inf = self.cumulative_probability["L_inf"][i]
+                    l_sup = self.cumulative_probability["L_sup"][i]
+                    qty = self.cumulative_probability["qty"][i]
+                    if num_pseudo >= l_inf and num_pseudo <= l_sup:
+                        self.data_simulation["dem_sim"].append(qty)
+                    else:
+                        pass
+                self.data_simulation["dem_adjust"].append(
+                    int(
+                        self.data_simulation["dem_sim"][y]
+                        * self.data_simulation["seasonal_factors"][y]
+                    )
+                )
+                if (
+                    self.data_simulation["inv_initial"][y]
+                    - self.data_simulation["dem_adjust"][y]
+                    > 0
+                ):
+                    self.data_simulation["inv_final"].append(
+                        self.data_simulation["inv_initial"][y]
+                        - self.data_simulation["dem_adjust"][y]
+                    )
+                    self.data_simulation["missing"].append(0)
+                else:
+                    self.data_simulation["inv_final"].append(0)
+                    self.data_simulation["missing"].append(
+                        (
+                            self.data_simulation["inv_initial"][y]
+                            - self.data_simulation["dem_adjust"][y]
+                        )
+                        * (-1)
+                    )
+                    self.missingQty += self.data_simulation["missing"][y]
+                new_inv = self.data_simulation["inv_final"][y]
+                # If R > Inv final
+                if self.data_simulation["inv_final"][y] < self.R:
+                    # If we have a order
+                    if self.orderActive == True:
+                        if self.limitMonth == self.counterMonth:
+                            self.orderActive = False
+                            self.limitMonth = 0
+                            self.counterMonth = 1
+                            new_inv = self.data_simulation["inv_final"][y] + self.Q
+                            new_inv = new_inv - self.missingQty
+                            self.missingQty = 0
+                            self.data_simulation["month_delivered"].append("---")
+                            self.data_simulation["Ri2"].append("---")
+                            self.data_simulation["order"].append(0)
+                        else:
+                            self.counterMonth += 1
+                            self.data_simulation["month_delivered"].append("---")
+                            self.data_simulation["Ri2"].append("---")
+                            self.data_simulation["order"].append(0)
+                    # If we not have a order
+                    else:
+                        randomRn = self.data_generator["Rn"][self.counterRn]
+                        self.counterRn += 1
+                        self.data_simulation["Ri2"].append(randomRn)
+                        for i in range(
+                            0, len(self.cumulative_probability_delivery["months"])
+                        ):
+                            l_inf = self.cumulative_probability_delivery["L_inf"][i]
+                            l_sup = self.cumulative_probability_delivery["L_sup"][i]
+                            if randomRn >= l_inf and randomRn <= l_sup:
+                                self.data_simulation["month_delivered"].append(
+                                    self.cumulative_probability_delivery["months"][i]
+                                )
+                                self.counterMonth = 1
+                                self.limitMonth = self.cumulative_probability_delivery[
+                                    "months"
+                                ][i]
+                                self.orderActive = True
+                                self.data_simulation["order"].append(1)
+                else:
+                    # If we have a order
+                    if self.orderActive == True:
+                        if self.limitMonth == self.counterMonth:
+                            self.orderActive = False
+                            self.limitMonth = 0
+                            self.counterMonth = 0
+                            new_inv = self.data_simulation["inv_final"][y] + self.Q
+                            new_inv = new_inv - self.missingQty
+                            self.missingQty = 0
+                            self.data_simulation["month_delivered"].append("---")
+                            self.data_simulation["Ri2"].append("---")
+                            self.data_simulation["order"].append(0)
+                        else:
+                            self.counterMonth += 1
+                            self.data_simulation["month_delivered"].append("---")
+                            self.data_simulation["Ri2"].append("---")
+                            self.data_simulation["order"].append(0)
+                    # If we not have a order
+                    else:
+                        self.data_simulation["Ri2"].append("---")
+                        self.data_simulation["month_delivered"].append("---")
+                        self.data_simulation["order"].append(0)
+
+                if self.data_simulation["inv_initial"][y] == 0:
+                    if self.data_simulation["inv_initial"][y] == 0:
+                        self.data_simulation["inv_monthly"].append(
+                            ((self.data_simulation["inv_final"][y]) / 2)
+                            * (
+                                self.data_simulation["inv_final"][y]
+                                / self.data_simulation["dem_adjust"][y]
+                            )
+                        )
+                elif self.data_simulation["inv_final"][y] == 0:
+                    if self.data_simulation["inv_final"][y] == 0:
+                        self.data_simulation["inv_monthly"].append(
+                            ((self.data_simulation["inv_initial"][y]) / 2)
+                            * (
+                                self.data_simulation["inv_initial"][y]
+                                / self.data_simulation["dem_adjust"][y]
+                            )
+                        )
+                elif (
+                    self.data_simulation["inv_final"][y] == 0
+                    and self.data_simulation["inv_initial"][y] == 0
+                ):
+                    self.data_simulation["inv_monthly"].append(0)
+                else:
+                    self.data_simulation["inv_monthly"].append(
+                        (
+                            self.data_simulation["inv_initial"][y]
+                            + self.data_simulation["inv_final"][y]
+                        )
+                        / 2
+                    )
+            df = pd.DataFrame(self.data_simulation)
+
+            print(df.to_string())
+            # print(self.data_simulation)
+        except Exception as err:
+            print(self.data_simulation)
+            print(str(err))
 
 
 A = inventory_system()
-A.calc_probability_delivery()
+A.Calc_probability_month()
+A.Calc_probability_delivery()
+A.Simulation()
--- Test Smirnov/Prueba_Smirnov.py
+++ Test Smirnov/Prueba_Smirnov.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import *
 from scripts.smirnov import Test_Smirnov
 
-
 main = tk.Tk()
 main.geometry("600x500")
 main.title("Prueba de kolmogorov-smirnov.")
@@ -11,7 +10,6 @@ main.resizable(False, False)
 Label1 = Label(main)
 Label2 = Label(main)
 Label3 = Label(main)
-
 
 def clickSolve():
     smirnov = Test_Smirnov(TxtAlpha.get(), int(txtN.get()))
@@ -23,26 +21,26 @@ def clickSolve():
 
 
 Label(main, text="Prueba de kolmogorov-smirnov",
-      font=("Arial", 14)).grid(row=0, column=0)
-Label(main, text="Inserte el numero de alpha: ").grid(row=1, column=0)
+      font=("Arial", 14)).grid(row=0, column=0, sticky='NW')
+Label(main, text="Inserte el numero de alpha: ").grid(row=1, column=0, sticky='NW')
 
 TxtAlpha = tk.Entry(main)
-TxtAlpha.grid(row=2, column=0)
+TxtAlpha.grid(row=2, column=0,sticky='NW')
 
 Label(main, text="Inserte el numero de numeros que desea utilizar: ").grid(
-    row=3, column=0)
+    row=3, column=0,sticky='NW')
 
 txtN = tk.Entry(main)
-txtN.grid(row=4, column=0)
+txtN.grid(row=4, column=0,sticky='NW')
 
 btnSolve = tk.Button(main, text="Calcular",
-                     command=clickSolve).grid(row=5, column=0)
+                     command=clickSolve).grid(row=5, column=0,sticky='NW')
 # ///*btnSolve.pack()*/
 
-# Label1.place(x=10, y=100)
+Label1.grid(sticky='NW')
 
-# Label2.place(x=10, y=110)
+Label2.grid(sticky='NW')
 
-# Label3.place(x=10, y=120)
+Label3.grid(sticky='NW')
 
 main.mainloop()
